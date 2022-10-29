07f5484c782c5bf457d8fd0b4a0c62307553d9b3
Armen Matevosyan <armen@matevosyan.am>
2022-10-29 05:27:37 +0000

    Sync LeetCode submission - Top K Frequent Words (python3)

--- problems/top_k_frequent_words/solution.py
+++ problems/top_k_frequent_words/solution.py
@@ -1,30 +1,10 @@
-from heapq import heappop, heappush
-
-class WordPair:
-    def __init__(self, word, count):
-        self.word = word
-        self.count = count
-
-    def __lt__(self, other):
-        if self.count != other.count:
-            return self.count > other.count
-        
-        return self.word < other.word
+from collections import Counter
+from heapq import nlargest
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        wordCount = defaultdict(int)
-        wordHeap = []
-
-        for word in words:
-            wordCount[word] += 1
-
-        for word in wordCount:
-            heappush(wordHeap, WordPair(word, wordCount[word]))
-        
-        results = []
-
-        for i in range(k):
-            results.append(heappop(wordHeap).word)
+        counter = Counter(words)
+        wordCount = sorted(counter.items())
+        results = [el[0] for el in nlargest(k, wordCount, key=lambda x: x[1])]
         
         return results
