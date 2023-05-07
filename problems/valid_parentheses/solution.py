afd383baa05e508c5b98a59ef9e245e9938bff8d
Armen Matevosyan <armen@matevosyan.am>
2023-05-07 13:13:45 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- problems/valid_parentheses/solution.py
+++ problems/valid_parentheses/solution.py
@@ -3,18 +3,15 @@ from collections import deque
 class Solution:
     def getOposite(self, s: str):
         maping = {
-            '(': ')',
             ')': '(',
-            '[': ']',
             ']': '[',
-            '{': '}',
-            '}': '{'
+            '}': '{',
         }
 
         return maping[s]
 
     def isOpen(self, s: str):
-        return s in ['(', '{', '[']
+        return s in ('(', '{', '[')
 
     def isValid(self, s: str) -> bool:
         q = deque()
@@ -22,12 +19,9 @@ class Solution:
         for el in s:
             if self.isOpen(el):
                 q.append(el)
-            else:
-                if len(q) == 0:
-                    return False
-                    
-                last = q.pop()
-                if last != self.getOposite(el):
-                    return False
+                continue
+            
+            if not q or q.pop() != self.getOposite(el):
+                return False
         
-        return len(q) == 0
+        return not q
