3d4d243ea74da37636c4a1d2073cafd73e84e1f2
Armen Matevosyan <armen@matevosyan.am>
2023-05-08 04:38:07 +0000

    Sync LeetCode submission - Balanced Binary Tree (python3)

--- problems/balanced_binary_tree/solution.py
+++ problems/balanced_binary_tree/solution.py
@@ -5,20 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def getHeight(self, root: Optional[TreeNode]):
+    def getHeight(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-
-        return max(self.getHeight(root.left), self.getHeight(root.right)) + 1
-
+        
+        return max(self.getHeight(
+            root.left), self.getHeight(root.right)) + 1
+    
     def isBalancedNodes(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
+        
+        diff = abs(self.getHeight(root.left) - self.getHeight(root.right))
 
-        if abs(self.getHeight(root.left) - self.getHeight(root.right)) > 1:
+        if diff > 1:
             return False
 
-        return self.isBalancedNodes(root.left) and self.isBalancedNodes(root.right) 
+        return self.isBalancedNodes(root.left) and self.isBalancedNodes(root.right)
+
 
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         return self.isBalancedNodes(root)
