461b6a3727eecc6b09ae3ac10683900705dedf7f
Armen Matevosyan <armen@matevosyan.am>
2023-05-20 16:48:10 +0000

    Sync LeetCode submission - Daily Temperatures (python3)

--- problems/daily_temperatures/solution.py
+++ problems/daily_temperatures/solution.py
@@ -5,11 +5,12 @@ class Solution:
         result = [0] * len(temperatures)
         remain = deque()
 
-        for i in range(len(temperatures) - 1):
+        for i in range(len(temperatures) - 1, -1, -1):
+            while remain and temperatures[remain[-1]] <= temperatures[i]:
+                remain.pop()
+            
+            result[i] = remain[-1] - i if remain else 0
             remain.append(i)
             
-            while remain and temperatures[remain[-1]] < temperatures[i + 1]:
-                ind = remain.pop()
-                result[ind] = i - ind + 1
         
         return result
