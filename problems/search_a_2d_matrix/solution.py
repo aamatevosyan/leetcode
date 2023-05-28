548141140e5eaa8c57ade811b85eb0c9120b0960
Armen Matevosyan <armen@matevosyan.am>
2023-05-28 10:11:43 +0000

    Sync LeetCode submission - Search a 2D Matrix (python3)

--- problems/search_a_2d_matrix/solution.py
+++ problems/search_a_2d_matrix/solution.py
@@ -1,37 +1,12 @@
+from bisect import bisect_left
+
 class Solution:
-    def searchColumn(self, matrix: List[List[int]], target: int) -> int:
-        l, r = 0, len(matrix) - 1
-
-        while l <= r:
-            m = (r - l) // 2 + l
-
-            if matrix[m][0] <= target:
-                l = m + 1
-            else:
-                r = m - 1
-        
-        if matrix[m][0] > target:
-            m -= 1
-        
-        return m
-    
-    def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            m = (r - l) // 2 + l
-
-            if nums[m] == target:
-                return m
-            elif nums[m] < target:
-                l = m + 1
-            else:
-                r = m - 1
-        
-        return -1
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        i = self.searchColumn(matrix, target)
-        print(i)
+        nums = []
+
+        for row in matrix:
+            for column in row:
+                nums.append(column)
         
-        return self.search(matrix[i], target) != -1
+        ind = bisect_left(nums, target)
+        return ind < len(nums) and nums[ind] == target
