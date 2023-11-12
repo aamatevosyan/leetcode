ac72ed3904b3b6cc083ce01d09854fa8bb29254d
Armen Matevosyan <armen@matevosyan.am>
2023-11-12 10:11:05 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- problems/course_schedule/solution.py
+++ problems/course_schedule/solution.py
@@ -1,34 +1,31 @@
-from collections import defaultdict
-
 class Solution:
+    def dfs(self, src: int, visited: List[int], graph: List[List[int]]):
+        if visited[src] == 1:
+            return True
+        
+        if visited[src] == -1:
+            return False
+        
+        visited[src] = -1
+
+        for v in graph[src]:
+            if not self.dfs(v, visited, graph):
+                return False
+        
+        visited[src] = 1
+
+        return True
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courseReqs = defaultdict(list)
+        visited = [0] * numCourses
+        graph = [[] for _ in range(numCourses)]
 
-        for a, b in prerequisites:
-            courseReqs[b].append(a)
+        for d, s in prerequisites:
+            graph[s].append(d)
         
-        indegree = [0] * numCourses
-
-        for v in courseReqs:
-            for u in courseReqs[v]:
-                indegree[u] += 1
+        for v in range(numCourses):
+            if not self.dfs(v, visited, graph):
+                return False
         
-        q = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
+        return True
         
-        cnt = 0
-
-        while q:
-            v = q.popleft()
-            indegree[v] -= 1
-            cnt += 1
-
-            for u in courseReqs[v]:
-                indegree[u] -= 1
-                if indegree[u] == 0:
-                    q.append(u)
-
-        return cnt == numCourses 
-
