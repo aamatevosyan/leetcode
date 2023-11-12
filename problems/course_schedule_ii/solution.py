3e7bb4cf693666011b8f8d801b84e146ef77b80c
Armen Matevosyan <armen@matevosyan.am>
2023-11-12 10:19:55 +0000

    Sync LeetCode submission - Course Schedule II (python3)

--- problems/course_schedule_ii/solution.py
+++ problems/course_schedule_ii/solution.py
@@ -1,29 +1,26 @@
-from collections import defaultdict, deque
+from collections import deque
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph, q, path = defaultdict(list), deque(), []
+        graph = [[] for _ in range(numCourses)]
+        path, indegree, q = [], [0] * numCourses, deque([])
 
-        for start, end in prerequisites:
-            graph[end].append(start)
-
-        indegree = [0] * numCourses
-        for start, end in prerequisites:
-            indegree[start] += 1    
-                
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.appendleft(i)
-
-        while q:
-            course = q.popleft()
-            path.append(course)
-
-            for i in graph[course]:
-                indegree[i] -= 1
-
-                if indegree[i] == 0:
-                    q.appendleft(i)
+        for d, s in prerequisites:
+            graph[s].append(d)
+            indegree[d] += 1
         
-        return path if len(path) == numCourses else []
-                
+        for v, degree in enumerate(indegree):
+            if degree == 0:
+                q.append(v)
+        
+        while q:
+            u = q.popleft()
+            path.append(u)
+
+            for v in graph[u]:
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    q.append(v)
+    
+        return [] if len(path) != numCourses else path
+        
