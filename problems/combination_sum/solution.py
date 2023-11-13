6040c6e9479f7c5ab2c4a3a8564acc3eae8cb599
Armen Matevosyan <armen@matevosyan.am>
2023-11-13 17:02:13 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,32 +1,19 @@
-
-
 class Solution:
-    def premutate(
-        self, 
-        candidates: List[int], 
-        combinations: List[int],
-        result: List[List[int]], 
-        target: int,
-        current: int,
-        index: int,
-    ):
-        if current == target:
-            result.append(combinations[:])
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+
+        self.helper(0, candidates, [], res, target)
         
-        if current >= target:
+        return res
+    
+    def helper(self, i: int, candidates: List[int], path: List[int], res: List[int], left: int) -> None:
+        if left == 0:
+            res.append(path)
+            return
+
+        if left < 0:
             return
         
-        for i in range(index, len(candidates)):
-            combinations.append(candidates[i])
-
-            self.premutate(candidates, combinations, result, target, current + candidates[i], i)
-
-            combinations.pop()
-
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        combinations, result = [], []
-
-        self.premutate(candidates, combinations, result, target, 0, 0)
-
-        return result
+        for j in range(i, len(candidates)):
+            self.helper(j, candidates, path + candidates[j:j+1], res, left - candidates[j])
