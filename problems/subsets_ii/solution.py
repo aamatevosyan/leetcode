bbbe22c13616359eb9811abd082c48ac43c503de
Armen Matevosyan <armen@matevosyan.am>
2023-11-13 15:55:15 +0000

    Sync LeetCode submission - Subsets II (python3)

---
+++ problems/subsets_ii/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        curr_set, result = [], []
+        
+        self.helper(0, nums, curr_set, result)
+
+        return result
+
+    def helper(self, i: int, nums: List[int], curr_set: List[int], result: List[int]) -> None:
+        if (i == len(nums)):
+            result.append(curr_set[:])
+            return
+        
+        curr_set.append(nums[i])
+        self.helper(i + 1, nums, curr_set, result)
+        curr_set.pop()
+
+        while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            i += 1
+
+        self.helper(i + 1, nums, curr_set, result)
+        
