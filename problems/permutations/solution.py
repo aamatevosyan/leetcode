253798e4fba02a86ac4e46943e6dafb6e8f98317
Armen Matevosyan <armen@matevosyan.am>
2023-11-13 20:07:16 +0000

    Sync LeetCode submission - Permutations (python3)

--- problems/permutations/solution.py
+++ problems/permutations/solution.py
@@ -1,24 +1,19 @@
 class Solution:
-    def helper(
-        self,
-        nums: List[int],
-        permutation: List[int],
-        rest: List[int],
-        result: List[List[int]],
-    ):
-        if len(rest) == 0:
-            result.append(permutation[:])
+    def helper(self, i: int, nums: List[int], perm: List[int], result: List[List[int]]) -> None:
+        if len(perm) == len(nums):
+            result.append(perm)
             return
         
-        for i, item in enumerate(rest):
-            permutation.append(item)
-            self.helper(nums, permutation, rest[:i] + rest[i + 1:], result)
-
-            permutation.pop()
+        for j in range(len(perm) + 1):
+            perm_copy = perm[:]
+            perm_copy.insert(j, nums[i])
+            self.helper(i + 1, nums, perm_copy, result)
 
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
-
-        self.helper(nums, [], nums[:], result)
+        
+        self.helper(0, nums, [], result)
 
         return result
+    
+        
