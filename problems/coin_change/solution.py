bf890b988f7207a2d87b388eecfa13164f13b753
Armen Matevosyan <armen@matevosyan.am>
2023-11-14 17:24:44 +0000

    Sync LeetCode submission - Coin Change (python3)

--- problems/coin_change/solution.py
+++ problems/coin_change/solution.py
@@ -1,27 +1,10 @@
-from collections import deque
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
+        dp = [0] + [math.inf] * amount
 
-        coins = sorted(coins, key=lambda x: -x)
-        memo = [-1] * (amount + 1)
-
-        q = deque([0])
-        memo[0] = 0
-
-        while q:
-            i = q.popleft()
-
-            for coin in coins:
-                if i + coin > amount or memo[i + coin] >= 0:
-                    continue
-
-                if i + coin == amount:
-                    return memo[i] + 1 
-                
-                q.append(i + coin)
-                memo[i + coin] = memo[i] + 1
-
-        return -1
+        for coin in coins:
+            for charge in range(coin, amount + 1):
+                dp[charge] = min(dp[charge], dp[charge - coin] + 1)
+        
+        return -1 if isinf(dp[amount]) else dp[amount]
+        
