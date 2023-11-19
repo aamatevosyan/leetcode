3172275bb18d210490916899bde9bb85c92a600b
Armen Matevosyan <armen@matevosyan.am>
2023-11-19 11:22:39 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- problems/minimum_window_substring/solution.py
+++ problems/minimum_window_substring/solution.py
@@ -1,31 +1,29 @@
-from collections import Counter
-
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        pattern_counts = Counter(t)
-        counter, start, min_len, min_substring =  len(pattern_counts), 0, len(s) + 1, ""
+        char_count = Counter(t)
+        distinct_chars, l, min_substr = len(char_count), 0, s[:] + " "
 
-        for end in range(len(s)):
-            if s[end] in pattern_counts:
-                pattern_counts[s[end]] -= 1
+        for r in range(len(s)):
+            if s[r] in char_count:
+                char_count[s[r]] -= 1
 
-                if pattern_counts[s[end]] == 0:
-                    counter -= 1
+                if char_count[s[r]] == 0:
+                    distinct_chars -= 1
             
-            while counter == 0:
-                if min_len > end - start + 1:
-                    min_len = end - start + 1
-                    min_substring = s[start: end + 1]
-
-                if s[start] in pattern_counts:
-                    pattern_counts[s[start]] += 1
+            while distinct_chars == 0 and l < len(s):
+                if r - l + 1 < len(min_substr):
+                    min_substr = s[l: r + 1]
                 
-                    if pattern_counts[s[start]] == 1:
-                        counter += 1
-                    
-                start += 1
+                if s[l] in char_count:
+                    char_count[s[l]] += 1
+
+                    if char_count[s[l]] == 1:
+                        distinct_chars += 1
+                
+                l += 1
         
-
-        return min_substring
-            
-
+        if len(min_substr) > len(s):
+            return ""
+        
+        return min_substr
+        
