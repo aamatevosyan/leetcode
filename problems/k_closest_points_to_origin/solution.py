a804f300e59e577eb964290822f931b5eb68f018
Armen Matevosyan <armen@matevosyan.am>
2023-11-19 15:50:14 +0000

    Sync LeetCode submission - K Closest Points to Origin (python3)

--- problems/k_closest_points_to_origin/solution.py
+++ problems/k_closest_points_to_origin/solution.py
@@ -1,19 +1,14 @@
-from queue import PriorityQueue
+from heapq import heappush, heappop
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        q = PriorityQueue()
+        q = []
 
         for x, y in points:
-            priority = x * x + y * y
-            q.put((-priority, (x, y)))
+            dist = x * x + y * y
+            heappush(q, (-dist, x, y))
 
-            if q.qsize() > k:
-                q.get()
-
-        result = []
-        for i in range(k):
-            _, point = q.get()
-            result.append(point)
+            if len(q) > k:
+                heappop(q)
         
-        return result
+        return [(x, y) for _, x, y in q]
