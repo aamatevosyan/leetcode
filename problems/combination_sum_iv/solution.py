873613d72b5328e6efefe666a66bbab1d5b85af9
Armen Matevosyan <armen@matevosyan.am>
2023-11-25 14:02:49 +0000

    Sync LeetCode submission - Combination Sum IV (python3)

--- problems/combination_sum_iv/solution.py
+++ problems/combination_sum_iv/solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)
-        dp[0] = 1
+        dp = [1] + [0] * target
 
-        nums.sort()
-
-        for current_target in range(1, target + 1):
-            for num in nums:
-                if num <= current_target:
-                    dp[current_target] += dp[current_target - num]
-                else:
+        for curr in range(1, target + 1):
+            for num in sorted(nums):
+                if num > curr:
                     break
+                
+                dp[curr] += dp[curr - num]
         
         return dp[target]
         
