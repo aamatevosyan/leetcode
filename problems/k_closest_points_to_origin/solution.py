3f6c3d82908c7b736837aadccf9f287350c68f53
Armen Matevosyan <armen@matevosyan.am>
2023-11-26 15:05:42 +0000

    Sync LeetCode submission - K Closest Points to Origin (python3)

--- problems/k_closest_points_to_origin/solution.py
+++ problems/k_closest_points_to_origin/solution.py
@@ -2,13 +2,11 @@ from heapq import heappush, heappop
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        q = []
+        q = [ (-(x * x + y * y), x, y) for x, y in points[:k]]
+        heapify(q)
 
-        for x, y in points:
+        for x, y in points[k:]:
             dist = x * x + y * y
-            heappush(q, (-dist, x, y))
-
-            if len(q) > k:
-                heappop(q)
+            heappushpop(q, (-dist, x, y))
         
         return [(x, y) for _, x, y in q]
