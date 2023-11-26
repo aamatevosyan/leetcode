ca028dba9265aab10864cd4dc58291597b8e41ba
Armen Matevosyan <armen@matevosyan.am>
2023-11-26 11:41:37 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,26 +1,23 @@
-from collections import deque
-
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+    directions = ((0, 1), (0, -1), (-1, 0), (1, 0))
+
+    def fill(self, image: List[List[int]], i: int, j: int, color: int, initial_color: int):
         n, m = len(image), len(image[0])
-        directions = ((0, 1), (0, -1), (-1, 0), (1, 0))
+        image[i][j] = color
+        
+        for di, dj in self.directions:
+             _i, _j = i + di, j + dj
 
-        q = deque()
-        q.append((sr, sc))
-
-        initialColor = image[sr][sc]
-
-        while q:
-            x, y = q.pop()
-            if image[x][y] != initialColor or image[x][y] == color:
-                continue
-
-            image[x][y] = color
-
-            for dx, dy in directions:
-                _x, _y = x + dx, y + dy
-
-                if 0 <= _x < n and 0 <= _y < m:
-                    q.append((_x, _y))
-
+             if _i < 0 or _i >= n or _j < 0 or _j >= m or image[_i][_j] != initial_color or image[_i][_j] == color:
+                 continue
+             
+             self.fill(image, _i, _j, color, initial_color)
+        
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if image[sr][sc] != color:
+            self.fill(image, sr, sc, color, image[sr][sc])
+        
         return image
+
+
+        
