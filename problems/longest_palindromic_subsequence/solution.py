345ebd349e9659162b82a4d585aabceb28ae51f5
Armen Matevosyan <armen@matevosyan.am>
2023-11-29 18:56:04 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (python3)

--- problems/longest_palindromic_subsequence/solution.py
+++ problems/longest_palindromic_subsequence/solution.py
@@ -1,18 +1,19 @@
 class Solution:
+    @lru_cache(None)
+    def helper(self, i: int, j: int, s: str) -> int:
+        if i == j:
+            return 1
+        
+        if i > j:
+            return 0
+        
+        if s[i] == s[j]:
+            return self.helper(i + 1, j - 1, s) + 2
+        
+        return max(
+            self.helper(i + 1, j, s),
+            self.helper(i, j - 1, s),
+        )
+
     def longestPalindromeSubseq(self, s: str) -> int:
-        n = len(s)
-        dp = [0] * n
-
-        for i in range(n - 1, -1, -1):
-            curr = [0] * i + [1] + [0] * (n - i - 1)
-
-            for j in range(i + 1, n):
-
-                if s[i] == s[j]:
-                    curr[j] = 2 + dp[j - 1]
-                else:
-                    curr[j] = max(dp[j], curr[j - 1])
-            
-            dp = curr
-
-        return dp[-1]
+        return self.helper(0, len(s) - 1, s)
