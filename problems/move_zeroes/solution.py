ab8ad78f145740bcaa554b200bc87b645e270555
Armen Matevosyan <armen@matevosyan.am>
2023-12-02 17:18:32 +0000

    Sync LeetCode submission - Move Zeroes (python3)

--- problems/move_zeroes/solution.py
+++ problems/move_zeroes/solution.py
@@ -3,13 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l, r = 0, 1
 
-        while r < len(nums):
-            if nums[l] == 0 and nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-            
-            if nums[l] != 0:
-                l += 1
-            
-            r += 1
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+        
+        while slow < len(nums):
+            nums[slow] = 0
+            slow += 1
+        
