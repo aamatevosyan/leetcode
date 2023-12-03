da6e1c047ab7791d69520e19afd82d3a5da00550
Armen Matevosyan <armen@matevosyan.am>
2023-12-03 19:21:04 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,19 +1,26 @@
 class Solution:
+    def __init__(self):
+        self.result = []
+        self.total = 0
+        self.path = []
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        candidates.sort()
+        self.backtrack(candidates, target, 0)
 
-        self.helper(0, candidates, [], res, target)
-        
-        return res
+        return self.result
     
-    def helper(self, i: int, candidates: List[int], path: List[int], res: List[int], left: int) -> None:
-        if left == 0:
-            res.append(path)
-            return
+    def backtrack(self, candidates: List[int], target: int, start: int) -> None:
+        if self.total == target:
+            self.result.append(self.path[:])
 
-        if left < 0:
-            return
-        
-        for j in range(i, len(candidates)):
-            self.helper(j, candidates, path + candidates[j:j+1], res, left - candidates[j])
+        if self.total >= target:
+            return    
+
+        for i in range(start, len(candidates)):
+            self.path.append(candidates[i])
+            self.total += candidates[i]
+
+            self.backtrack(candidates, target, i)
+
+            self.total -= candidates[i]
+            self.path.pop()
