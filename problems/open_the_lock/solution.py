ec223db155ac5d8929ff1fbf61fc284097fca217
Armen Matevosyan <armen@matevosyan.am>
2023-12-04 19:59:06 +0000

    Sync LeetCode submission - Open the Lock (python3)

--- problems/open_the_lock/solution.py
+++ problems/open_the_lock/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     START_NODE = "0000"
 
-    def neighbors(self, node: str):
+    def comibnations(self, node: str):
         for i in range(len(node)):
             digit = int(node[i])
 
@@ -12,24 +12,27 @@ class Solution:
     
     def openLock(self, deadends: List[str], target: str) -> int:
         dead = set(deadends)
-        level, begin, end = 0, set([self.START_NODE]), set([target])
+        level, q1, q2 = 0, set([self.START_NODE]), set([target])
 
-        while begin and end:
-            neighbors = set()
-            for node in begin:
+        while q1 and q2:
+            q = set()
+            if len(q1) > len(q2):
+                q1, q2 = q2, q1
+
+            for node in q1:
                 if node in dead:
                     continue
                 
-                if node in end:
+                if node in q2:
                     return level
                 
                 dead.add(node)
-                for neighbor in self.neighbors(node):
-                    neighbors.add(neighbor)
+                for comibnation in self.comibnations(node):
+                    q.add(comibnation)
 
             level += 1
-            begin = end
-            end = neighbors
+            q1 = q2
+            q2 = q
         
         return -1
 
