42fc10647260e7689f8be7764e6bd3cfc305d619
Armen Matevosyan <armen@matevosyan.am>
2023-12-05 07:29:35 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,10 +1,42 @@
 class Solution:
+    def left_bound(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] == target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return l if l < len(nums) and nums[l] == target else -1
+
+    def right_bound(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] == target:
+                l = m + 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return r if r >= 0 and nums[r] == target else -1
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left_ind = bisect_left(nums, target)
-        if left_ind == len(nums) or nums[left_ind] != target:
+        l, r = 0, len(nums) - 1
+
+        min_l = self.left_bound(nums, target)
+        
+        if min_l == -1:
             return [-1, -1]
         
-        right_ind = bisect_right(nums, target) - 1
+        max_r = self.right_bound(nums, target)
         
-        return[left_ind, right_ind]
+        return [min_l, max_r]
+
         
