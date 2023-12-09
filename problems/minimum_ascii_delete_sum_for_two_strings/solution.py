73cd7590a3d1153b96a8cae20ab4fbdc717bbb28
Armen Matevosyan <armen@matevosyan.am>
2023-12-09 18:33:09 +0000

    Sync LeetCode submission - Minimum ASCII Delete Sum for Two Strings (python3)

---
+++ problems/minimum_ascii_delete_sum_for_two_strings/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCommonSubsequenceOrd(self, text1: str, text2: str) -> int:
+        if len(text1) < len(text2):
+            text1, text2 = text2, text1
+        
+        n, m = len(text1), len(text2)
+        dp = [0] * (m + 1)
+
+        for i in range(1, n + 1):
+            curr = [0] * (m + 1)
+
+            for j in range(1, m + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    curr[j] = ord(text1[i - 1]) + dp[j - 1]
+                else:
+                    curr[j] = max(dp[j], curr[j - 1])
+            
+            dp = curr
+        
+        return dp[m]
+
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        return sum(map(ord, s1)) + sum(map(ord, s2)) - 2 * self.longestCommonSubsequenceOrd(s1, s2)
+        
