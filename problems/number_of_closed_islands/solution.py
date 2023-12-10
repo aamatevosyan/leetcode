9b01c8ca1c81e9e277cf0e5a5da71dc653cf052c
Armen Matevosyan <armen@matevosyan.am>
2023-12-10 10:24:48 +0000

    Sync LeetCode submission - Number of Closed Islands (python3)

---
+++ problems/number_of_closed_islands/solution.py
@@ -0,0 +1,35 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        if len(grid) == 0:
+            return 0
+
+        m, n, cnt = len(grid), len(grid[0]), 0
+
+        for i in range(m):
+            self.dfs(grid, m, n, i, 0)
+            self.dfs(grid, m, n, i, n - 1)
+        
+        for j in range(n):
+            self.dfs(grid, m, n, 0, j)
+            self.dfs(grid, m, n, m - 1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    continue
+                
+                self.dfs(grid, m, n, i, j)
+                cnt += 1
+        
+        return cnt
+
+    def dfs(self, grid, m, n, i, j):
+        if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 1:
+            return
+        
+        grid[i][j] = 1
+
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+        for _i, _j in directions:
+            self.dfs(grid, m, n, i + _i, j + _j)
