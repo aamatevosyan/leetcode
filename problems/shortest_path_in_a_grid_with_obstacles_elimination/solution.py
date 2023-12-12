7799e79e9a36709b7d8983f5e518e61925a2ee6e
Armen Matevosyan <armen@matevosyan.am>
2023-12-12 19:35:03 +0000

    Sync LeetCode submission - Shortest Path in a Grid with Obstacles Elimination (python3)

--- problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.py
+++ problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.py
@@ -14,36 +14,33 @@ class Solution:
         if k >= m + n - 2:
             return m + n - 2
         
-        q, visited, current_steps = deque([(0, 0, k)]), set(), -1
+        q, visited, steps = deque([(0, 0, k)]), set(), 0
 
         while q:
-            _q = deque()
-            current_steps += 1
-
-            while q:
-                i, j, available_steps = q.popleft()
-
-                if i == m - 1 and j == n - 1:
-                    return current_steps
-                
+            for _ in range(len(q)):
+                i, j, steps_left = q.popleft()
+                                
                 for d_i, d_j in self.DIRECTIONS:
                     _i, _j = i + d_i, j + d_j
 
                     if _i < 0 or _i >= m or _j < 0 or _j >= n:
                         continue
                     
-                    if available_steps - grid[_i][_j] < 0:
+                    if steps_left - grid[_i][_j] < 0:
                         continue
+                    
+                    if _i == m - 1 and _j == n - 1:
+                        return steps + 1
 
-                    step = (_i, _j, available_steps - grid[_i][_j])
+                    option = (_i, _j, steps_left - grid[_i][_j])
                     
-                    if step in visited:
+                    if option in visited:
                         continue
                     
-                    visited.add(step)
-                    _q.append(step)
+                    visited.add(option)
+                    q.append(option)
             
-            q = _q
+            steps += 1
 
         return -1
         
